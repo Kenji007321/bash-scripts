e41fcc467fd5c77e1e8465640cfa5395972c1ece
Kenji007321 <108785825+Kenji007321@users.noreply.github.com>
2024-05-26 22:16:10 +0900

    Update cruft_remove.sh

--- cruft_remove.sh
+++ cruft_remove.sh
@@ -15,79 +15,14 @@ read -p "How many days is too old?: " old
 
 readarray -t f < <(find "${folder}" -maxdepth 1 -type f -mtime +"${old}")
 
-readarray -t wcf < <(printf "%s\n" "${f[@]}" 2> /dev/null | wc -l)
+readarray -t wcf < <(printf "%s" "${f[@]}" 2> /dev/null | wc -l)
 
 readarray -t fn < <(printf "%s\n" "${f[@]}")
 
-if [[ "${wcf[@]}" -eq 1 ]] && [[ $(printf "%s" "${f[@]}" | wc -l) -eq 0 ]]
-then
-        wcf2=$(( "${wcf[@]}" - 1 ))
-else
-        readarray -t wcf2 < <(printf "%s\n" "${f[@]}" 2> /dev/null | wc -l)
-fi
-
-if [[ "${wcf2[@]}" -gt 0 ]]
+if [[ "${wcf[@]}" -gt 0 ]]
 then
         echo " "
-        echo "You have ${wcf2[@]} file(s) to be removed"
-
-        echo " "
-
-        echo "The file(s) below was last modified more than ${old} days ago: "
-
-        printf ">%s\n" "${fn[@]}"
-        echo " "
-
-#Select command below
-
-        PS3="Do you want to delete the file(s)?: "
-        select options in  yes no
-        do
-        if [[ $options = yes ]]
-        then
-                echo " "
-                echo "You have selected \"yes\""
-        sleep 1
-                echo " "
-                echo "Removing the following files..."
-                echo "${fn[@]}"
-                for file in "${fn[@]}"; do
-                rm -vf "${file}" 2> /dev/null
-        done
-        sleep 1
-        if [[ $? = 0 
-        #!/bin/bash
-
-read -p "Enter folder for cruft cleanup (\".\" for current directory): " folder
-
-if [[ ! -e ${folder} ]]
-then
-        echo " "
-        echo "Directory doesn't exist."
-sleep 1
-        echo "Aborting..."
-        exit
-fi
-
-read -p "How many days is too old?: " old
-
-readarray -t f < <(find "${folder}" -maxdepth 1 -type f -mtime +"${old}")
-
-readarray -t wcf < <(printf "%s\n" "${f[@]}" 2> /dev/null | wc -l)
-
-readarray -t fn < <(printf "%s\n" "${f[@]}")
-
-if [[ "${wcf[@]}" -eq 1 ]] && [[ $(printf "%s" "${f[@]}" | wc -l) -eq 0 ]]
-then
-        wcf2=$(( "${wcf[@]}" - 1 ))
-else
-        readarray -t wcf2 < <(printf "%s\n" "${f[@]}" 2> /dev/null | wc -l)
-fi
-
-if [[ "${wcf2[@]}" -gt 0 ]]
-then
-        echo " "
-        echo "You have ${wcf2[@]} file(s) to be removed"
+        echo "You have ${wcf[@]} file(s) to be removed"
 
         echo " "
 
@@ -142,6 +77,4 @@ else
         sleep 1
         exit 0
 fi
-
-
-
+        
